4e0d05ac0e3e11dcd9bc4731e16451040be5be8e
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-09-08 14:28:54 -0300

    Get all of the elements with the class you assigned and save them to a variable. Console.log that variable.

--- dom_practice.js
+++ dom_practice.js
@@ -6,3 +6,6 @@ var h2 = document.querySelector("h2")
 console.log(h2)
 
 h2.innerText = "welcome to São Paulo!!!" 
+
+let elements = document.getElementsByClassName("element")
+console.log(elements)
